4f6e86cef583366301f3fb874e74df3311751e15
Jeff Manian <jeffrey.manian@gmail.com>
2021-03-06 21:22:41 -0500

    Team class

---
+++ app/lib/team.rb
@@ -0,0 +1,29 @@
+class Team
+  attr_reader :tricode, :city, :name
+
+  def initialize(tricode, city, name)
+    @tricode = tricode
+    @city = city
+    @name = name
+  end
+
+  def full_name
+    [city, name].join(' ')
+  end
+
+  TEAMS = [
+    [:nyk, 'New York', 'Knicks'],
+    [:chi, 'Chicago', 'Bulls']
+  ].map { |tc, c, n| [tc, Team.new(tc, c, n)] }
+          .to_h.with_indifferent_access
+
+  class << self
+    def tricodes
+      TEAMS.keys
+    end
+
+    def [](tricode)
+      TEAMS[tricode]
+    end
+  end
+end
