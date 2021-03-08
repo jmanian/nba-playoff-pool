ad9a16d34e082c1bc03c3189e33737ac2b1761ba
Jeff Manian <jeffrey.manian@gmail.com>
2021-03-07 21:49:39 -0500

    add all teams

--- app/lib/team.rb
+++ app/lib/team.rb
@@ -12,8 +12,36 @@ class Team
   end
 
   TEAMS = [
+    [:atl, 'Atlanta', 'Hawks'],
+    [:bkn, 'Brooklyn', 'Nets'],
+    [:bos, 'Boston', 'Celtics'],
+    [:cha, 'Charlotte', 'Hornets'],
+    [:chi, 'Chicago', 'Bulls'],
+    [:cle, 'Cleveland', 'Cavaliers'],
+    [:dal, 'Dallas', 'Mavericks'],
+    [:den, 'Denver', 'Nuggets'],
+    [:det, 'Detroit', 'Pistons'],
+    [:gsw, 'Golden State', 'Warriors'],
+    [:hou, 'Houston', 'Rockets'],
+    [:ind, 'Indiana', 'Pacers'],
+    [:lac, 'Los Angeles', 'Clippers'],
+    [:lal, 'Los Angeles', 'Lakers'],
+    [:mem, 'Memphis', 'Grizzlies'],
+    [:mia, 'Miami', 'Heat'],
+    [:mil, 'Milwaukee', 'Bucks'],
+    [:min, 'Minnesota', 'Timberwolves'],
+    [:nop, 'New Orleans', 'Pelicans'],
     [:nyk, 'New York', 'Knicks'],
-    [:chi, 'Chicago', 'Bulls']
+    [:okc, 'Oklahoma City', 'Thunder'],
+    [:orl, 'Orlando', 'Magic'],
+    [:phi, 'Philadelphia', '76ers'],
+    [:phx, 'Phoenix', 'Suns'],
+    [:por, 'Portland', 'Trail Blazers'],
+    [:sac, 'Sacramento', 'Kings'],
+    [:sas, 'San Antonio', 'Spurs'],
+    [:tor, 'Toronto', 'Raptors'],
+    [:uta, 'Utah', 'Jazz'],
+    [:was, 'Washington', 'Wizards'],
   ].map { |tc, c, n| [tc, Team.new(tc, c, n)] }
           .to_h.with_indifferent_access
 
