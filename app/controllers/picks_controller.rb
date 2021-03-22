c3c2476ca42a140f87cb5ac7679212cb3a3ab906
Jeff Manian <jeffrey.manian@gmail.com>
2021-03-22 00:00:29 -0400

    picks new/create and better index layout

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -3,7 +3,8 @@ class PicksController < ApplicationController
 
   def index
     @picks = current_user.picks.includes(:matchup)
-    @other_matchups = Matchup.where(year: 2021).where.not(id: @picks.map(&:matchup_id)).accepting_entries
+    @other_matchups = Matchup.where(year: 2021).where.not(id: @picks.map(&:matchup_id)).accepting_entries.to_a
+    @accepting_picks = Matchup.accepting_entries.exists?
     @picks = @picks.group_by { |pick| pick.matchup.round }
   end
 
@@ -13,6 +14,16 @@ class PicksController < ApplicationController
   end
 
   def create
+    valid_matchup_ids = Matchup.accepting_entries.ids
+    params.require(:pick).permit(matchup: :result).to_h[:matchup].each do |matchup_id, pick_data|
+      matchup_id = matchup_id.to_i
+      next unless valid_matchup_ids.include?(matchup_id)
+
+      result = pick_data[:result]
+      pick = current_user.picks.find_or_initialize_by(matchup_id: matchup_id)
+      pick.result = result
+      pick.save!
+    end
     redirect_to action: :index
   end
 end
