4b96a6a342b0002998729e5ec5badee1acd9cc18
Jeff Manian <jeffrey.manian@gmail.com>
2021-09-30 00:34:17 -0400

    beginning of a standings table

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -1,3 +1,30 @@
 class StandingsController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
+
+  def index
+    users = User.includes(picks: :matchup)
+
+    @data = users.map do |user|
+      picks = user.picks
+                  .select { |p| p.matchup.started? }
+                  .group_by { |p| p.matchup.round }
+
+      round_scores = picks.transform_values do |picks|
+        [
+          picks.sum(&:min_points),
+          picks.sum(&:potential_points)
+        ]
+      end
+      round_scores.default = [0, 0]
+
+      totals = [
+        round_scores.values.map(&:first).sum,
+        round_scores.values.map(&:last).sum
+      ]
+
+      [user, round_scores, totals]
+    end
+
+    @rounds = @data.map(&:second).flat_map { |user_scores| user_scores.keys }.uniq.sort
+  end
 end
