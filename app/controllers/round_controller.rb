09cef1679811556b2930e618f2ba90bee1192ab9
Jeff Manian <jmanian@users.noreply.github.com>
2021-10-05 23:43:39 -0400

    add round summaries (#26)

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -18,9 +18,18 @@ class RoundController < ApplicationController
                      .sort
 
     @round_name = @matchups.first.round_name
+    @num_outcomes = @matchups.first.games_needed_to_win * 2
 
     @user_data = picks.group_by(&:user)
                       .transform_values { |user_picks| user_picks.index_by(&:matchup_id) }
+
+    @num_users = @user_data.length
+
+    @matchup_data = picks.group_by(&:matchup_id)
+                         .transform_values do |pps|
+                           pps.group_by(&:scoring_index)
+                              .transform_values(&:length)
+                         end
   end
   # rubocop:enable Metrics
 end
