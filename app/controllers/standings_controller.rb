4f79dbb930a8d6f1c6df5b78654b0d5a2ac3c1fc
Jeff Manian <jeffrey.manian@gmail.com>
2021-10-08 10:12:11 -0400

    remove chart.js

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -31,46 +31,6 @@ class StandingsController < ApplicationController
     end
 
     @rounds = @data.map(&:second).flat_map(&:keys).uniq.sort
-
-    @chart_labels = @data.map { |user, _, _| user.username }
-
-    @chart_data = @rounds.flat_map do |round_num|
-      round_name = @round_names[round_num]
-      user_round_scores = @data.map(&:second).map { |rs| rs[round_num] }
-      mins = user_round_scores.map(&:first)
-      pots = user_round_scores.map(&:second)
-      if pots.any?(&:positive?)
-        [
-          {
-            label: "#{round_name} Definite",
-            backgroundColor: background_color(round_num),
-            data: mins
-          },
-          {
-            label: "#{round_name} Potential",
-            backgroundColor: 'rgba(180, 183, 187)',
-            data: pots
-          }
-        ]
-      else
-        [
-          {
-            label: round_name,
-            backgroundColor: background_color(round_num),
-            data: mins
-          }
-        ]
-      end
-    end
   end
   # rubocop:enable Metrics
-
-  def background_color(round_num)
-    [
-      'rgba(51, 101, 138)',
-      'rgba(215, 129, 106)',
-      'rgba(117, 142, 79)',
-      'rgba(134, 187, 216)'
-    ][round_num - 1]
-  end
 end
