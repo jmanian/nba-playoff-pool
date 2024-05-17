9737f9ce91d3f02b7c9057dd9d083f0497f8cc96
Jeff Manian <jeffrey.manian@gmail.com>
2024-05-16 23:52:56 -0400

    Overhaul Nav (#113)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :load_all_seasons
+  before_action :load_past_seasons
 
   BG_COLORS = %w[
     not-used
@@ -17,22 +17,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
-  def load_all_seasons
-    @all_season_rounds =
+  def load_past_seasons
+    @past_seasons =
       Matchup.started
-        .select(:sport, :year, :round)
         .distinct
-        .group_by { |m| [m.sport.to_sym, m.year] }
-        .transform_values { |matchups| matchups.to_h { |m| [m.round, m.round_name] } }
-
-    # The highest round number of the current season, for the rounds button on navbar
-    @current_last_round = @all_season_rounds[CurrentSeason.sport_year]&.sort&.last&.first
-    # Prior seasons and their last round number, for the past seasons navbar dropdown
-    @past_seasons = @all_season_rounds.reject { |sport_year, _| sport_year == CurrentSeason.sport_year }
-      .map { |sport_and_year, rounds| [*sport_and_year, rounds.keys.max] }
-  end
-
-  def set_round_names
-    @round_names = @all_season_rounds[[params[:sport].to_sym, params[:year].to_i]]
+        .order(:sport, year: :desc)
+        .pluck(:sport, :year)
+        .map { |sport, year| [sport.to_sym, year] }
+        .reject { |sy| sy == CurrentSeason.sport_year }
+        .group_by { |sport, year| sport }
+        .transform_values { |sport_years| sport_years.map { |sport, year| year } }
   end
 end
