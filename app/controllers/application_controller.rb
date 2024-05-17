c84f365411312d8f7ba3426cf6b185be0b4affdb
Jeff Manian <jeffrey.manian@gmail.com>
2024-05-16 23:38:00 -0400

    improve past seasons with sport headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :load_all_seasons
+  before_action :load_past_seasons
 
   BG_COLORS = %w[
     not-used
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
-  def load_all_seasons
+  def load_past_seasons
     @past_seasons =
       Matchup.started
         .distinct
@@ -25,5 +25,7 @@ class ApplicationController < ActionController::Base
         .pluck(:sport, :year)
         .map { |sport, year| [sport.to_sym, year] }
         .reject { |sy| sy == CurrentSeason.sport_year }
+        .group_by { |sport, year| sport }
+        .transform_values { |sport_years| sport_years.map { |sport, year| year } }
   end
 end
