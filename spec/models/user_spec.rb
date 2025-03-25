29a975766f14ca835488528af52a5ba4440522f2
Jeff Manian <jeffrey.manian@gmail.com>
2025-03-24 23:30:09 -0400

    One test and some nocov

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#title" do
+    subject { user.title }
+
+    let(:user) { create :user }
+
+    it { should eql user.email }
+  end
 end
