93e4c45e8300776f3b0aff0aeb421958df705e0e
Jeff Manian <jeffrey.manian@gmail.com>
2025-03-24 22:56:30 -0400

    test nba client using vcr

--- spec/lib/sync/nba/client_spec.rb
+++ spec/lib/sync/nba/client_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe Sync::Nba::Client do
-  pending
+  describe "::fetch_bracket", :vcr do
+    subject { described_class.fetch_bracket(year) }
+
+    context "for the 2024 playoffs" do
+      let(:year) { 2024 }
+
+      it "returns the JSON from the 2023 bracket endpoint" do
+        should include(:meta, :bracket)
+        expect(subject[:bracket][:seasonYear]).to eql((year - 1).to_s)
+      end
+    end
+
+    context "for the 2023 playoffs" do
+      let(:year) { 2023 }
+
+      it "returns the JSON from the 2022 bracket endpoint" do
+        should include(:meta, :bracket)
+        expect(subject[:bracket][:seasonYear]).to eql((year - 1).to_s)
+      end
+    end
+  end
 end
