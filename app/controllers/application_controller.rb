f69eae159595cc7080f84c0e0bfebb56b12fa04c
Jeff Manian <jeffrey.manian@gmail.com>
2025-03-24 23:31:34 -0400

    One test and some nocov (#122)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,12 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # :nocov:
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+  # :nocov:
 
   def load_past_seasons
     @past_seasons =
