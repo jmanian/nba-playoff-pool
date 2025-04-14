93205a669609df60af5e9371c0c9e70da5fec107
Jeff Manian <jeffrey.manian@gmail.com>
2025-04-14 11:20:59 -0400

    add tests for emppty state

--- spec/controllers/standings_controller_spec.rb
+++ spec/controllers/standings_controller_spec.rb
@@ -4,18 +4,7 @@ require "controller_helper"
 describe StandingsController, type: :controller do
   describe "#index" do
     let(:users) { create_list :user, 10 }
-    let!(:matchups) do
-      [
-        *(1..4).flat_map do |n|
-          [
-            create(:matchup, :nba, :started, year: 2025, conference: "east", round: 1, number: n),
-            create(:matchup, :nba, :started, year: 2025, conference: "west", round: 1, number: n)
-          ]
-        end,
-        create(:matchup, :nba, :started, year: 2025, conference: "east", round: 2, number: 1),
-        create(:matchup, :nba, :started, year: 2025, conference: "west", round: 2, number: 1)
-      ]
-    end
+
     let!(:other_matchups) do
       [
         create(:matchup, :nba, :accepting_entries, year: 2025, conference: "east", round: 2, number: 2),
@@ -25,26 +14,57 @@ describe StandingsController, type: :controller do
       ]
     end
 
-    let!(:picks) do
-      users.product(matchups).map do |user, matchup|
-        create(:pick, user: user, matchup: matchup)
-      end
-    end
-
     let!(:other_picks) do
       users.product(other_matchups).map do |user, matchup|
         create(:pick, user: user, matchup: matchup)
       end
     end
 
-    it "renders the index template" do
-      get :index, params: {sport: "nba", year: 2025}
-      expect(response).to render_template(:index)
-      expect(assigns(:matchup_data).keys).to match_array(matchups.map(&:id))
-      expect(assigns(:rounds)).to eq([1, 2])
-      expect(assigns(:data).map(&:user)).to match_array(users)
-      expect(assigns(:data).flat_map(&:picks)).to match_array(picks)
-      expect(assigns(:rounds_data).map { |r| r[:number] }).to eq([1, 2])
+    context "when there are no matchups to display" do
+      it "renders the empty template" do
+        get :index, params: {sport: "nba", year: 2025}
+        expect(response).to render_template(:empty)
+      end
+    end
+
+    context "when there are matchups to display" do
+      let!(:matchups) do
+        [
+          *(1..4).flat_map do |n|
+            [
+              create(:matchup, :nba, :started, year: 2025, conference: "east", round: 1, number: n),
+              create(:matchup, :nba, :started, year: 2025, conference: "west", round: 1, number: n)
+            ]
+          end,
+          create(:matchup, :nba, :started, year: 2025, conference: "east", round: 2, number: 1),
+          create(:matchup, :nba, :started, year: 2025, conference: "west", round: 2, number: 1)
+        ]
+      end
+
+      context "but no picks" do
+        it "renders the empty template" do
+          get :index, params: {sport: "nba", year: 2025}
+          expect(response).to render_template(:empty)
+        end
+      end
+
+      context "and there are picks" do
+        let!(:picks) do
+          users.product(matchups).map do |user, matchup|
+            create(:pick, user: user, matchup: matchup)
+          end
+        end
+
+        it "renders the index template" do
+          get :index, params: {sport: "nba", year: 2025}
+          expect(response).to render_template(:index)
+          expect(assigns(:matchup_data).keys).to match_array(matchups.map(&:id))
+          expect(assigns(:rounds)).to eq([1, 2])
+          expect(assigns(:data).map(&:user)).to match_array(users)
+          expect(assigns(:data).flat_map(&:picks)).to match_array(picks)
+          expect(assigns(:rounds_data).map { |r| r[:number] }).to eq([1, 2])
+        end
+      end
     end
   end
 end
